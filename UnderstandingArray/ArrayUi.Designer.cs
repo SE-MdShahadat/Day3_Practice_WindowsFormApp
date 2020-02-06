11a35bfab1bbe4efe617551541891e580a65b67a
SE-MdShahadat <se.mdshahadat@gmail.com>
2020-02-06 14:26:45 +0600

    array_iteration

---
+++ UnderstandingArray/ArrayUi.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace UnderstandingArray
+{
+    partial class ArrayUi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.displayRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // displayRichTextBox
+            // 
+            this.displayRichTextBox.Location = new System.Drawing.Point(31, 156);
+            this.displayRichTextBox.Name = "displayRichTextBox";
+            this.displayRichTextBox.Size = new System.Drawing.Size(735, 242);
+            this.displayRichTextBox.TabIndex = 0;
+            this.displayRichTextBox.Text = "";
+            // 
+            // ArrayUi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.displayRichTextBox);
+            this.Name = "ArrayUi";
+            this.Text = "Array Practice";
+            this.Load += new System.EventHandler(this.ArrayUi_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox displayRichTextBox;
+    }
+}
+
---
+++ UnderstandingArray/ArrayUi.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UnderstandingArray
+{
+    public partial class ArrayUi : Form
+    {
+        public ArrayUi()
+        {
+            InitializeComponent();
+        }
+
+        private void ArrayUi_Load(object sender, EventArgs e)
+        {
+            int[] number = new int[10];
+            number[1] = 10;
+            number[2] = 20;
+            number[9] = 30;
+
+            string messsage = null;
+            for (int index=0; index< number.Length; index++)
+            {
+                if(number[index]!=0)
+                messsage = messsage + "Value of Index " + index + " is: " + number[index] + "\n";
+            }
+            displayRichTextBox.Text = messsage;
+        }
+    }
+}
