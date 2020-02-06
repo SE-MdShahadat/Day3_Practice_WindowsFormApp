6e8fdb948b2b84f419cdbdec094bb2a158de31cb
SE-MdShahadat <se.mdshahadat@gmail.com>
2020-02-06 16:38:06 +0600

    element_added_and_showed

--- UnderstandingArray/ArrayUi.Designer.cs
+++ UnderstandingArray/ArrayUi.Designer.cs
@@ -29,32 +29,69 @@
         private void InitializeComponent()
         {
             this.displayRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.ShowButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // displayRichTextBox
             // 
-            this.displayRichTextBox.Location = new System.Drawing.Point(31, 156);
+            this.displayRichTextBox.Location = new System.Drawing.Point(31, 117);
             this.displayRichTextBox.Name = "displayRichTextBox";
-            this.displayRichTextBox.Size = new System.Drawing.Size(735, 242);
+            this.displayRichTextBox.Size = new System.Drawing.Size(735, 281);
             this.displayRichTextBox.TabIndex = 0;
             this.displayRichTextBox.Text = "";
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(31, 75);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(252, 22);
+            this.inputTextBox.TabIndex = 1;
+            // 
+            // AddButton
+            // 
+            this.AddButton.Location = new System.Drawing.Point(290, 73);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(75, 24);
+            this.AddButton.TabIndex = 2;
+            this.AddButton.Text = "Add";
+            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // ShowButton
+            // 
+            this.ShowButton.Location = new System.Drawing.Point(691, 76);
+            this.ShowButton.Name = "ShowButton";
+            this.ShowButton.Size = new System.Drawing.Size(75, 22);
+            this.ShowButton.TabIndex = 3;
+            this.ShowButton.Text = "Show";
+            this.ShowButton.UseVisualStyleBackColor = true;
+            this.ShowButton.Click += new System.EventHandler(this.ShowButton_Click);
             // 
             // ArrayUi
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ShowButton);
+            this.Controls.Add(this.AddButton);
+            this.Controls.Add(this.inputTextBox);
             this.Controls.Add(this.displayRichTextBox);
             this.Name = "ArrayUi";
             this.Text = "Array Practice";
             this.Load += new System.EventHandler(this.ArrayUi_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.RichTextBox displayRichTextBox;
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.Button ShowButton;
     }
 }
 
--- UnderstandingArray/ArrayUi.cs
+++ UnderstandingArray/ArrayUi.cs
@@ -12,25 +12,57 @@ namespace UnderstandingArray
 {
     public partial class ArrayUi : Form
     {
+        
         public ArrayUi()
         {
             InitializeComponent();
         }
-
+        int size = 4;
+        int index = 0;
+        int[] number; 
         private void ArrayUi_Load(object sender, EventArgs e)
         {
-            int[] number = new int[10];
-            number[1] = 10;
-            number[2] = 20;
-            number[9] = 30;
+            //int[] number = new int[10];
+            //number[1] = 10;
+            //number[2] = 20;
+            //number[9] = 30;
 
-            string messsage = null;
-            for (int index=0; index< number.Length; index++)
+            //string messsage = null;
+            //for (int index=0; index< number.Length; index++)
+            //{
+            //    if(number[index]!=0)
+            //    messsage = messsage + "Value of Index " + index + " is: " + number[index] + "\n";
+            //}
+            //displayRichTextBox.Text = messsage;
+            number = new int[size];
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            displayRichTextBox.Clear();
+           
+                if (index != size)
+                {
+                    number[index] = Convert.ToInt32(inputTextBox.Text);
+                    displayRichTextBox.Text = "Value " + inputTextBox.Text + " is added to storage";
+                    index++;
+                }
+                else displayRichTextBox.Text = "Storage is full";
+                    
+            
+        }
+
+        private void ShowButton_Click(object sender, EventArgs e)
+        {
+            string message = "Elements are: ";
+            for(int index=0; index<size; index++)
             {
-                if(number[index]!=0)
-                messsage = messsage + "Value of Index " + index + " is: " + number[index] + "\n";
+                if (number[index] != null)
+                {
+                    message = message + number[index]+ " ";
+                }
+                displayRichTextBox.Text = message;
             }
-            displayRichTextBox.Text = messsage;
         }
     }
 }
