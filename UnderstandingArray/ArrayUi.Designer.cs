76467a5c2f34d44d80c6a64bb3d20551e523cf16
SE-MdShahadat <se.mdshahadat@gmail.com>
2020-02-18 22:06:04 +0600

    All_feature_added

--- UnderstandingArray/ArrayUi.Designer.cs
+++ UnderstandingArray/ArrayUi.Designer.cs
@@ -32,26 +32,40 @@
             this.inputTextBox = new System.Windows.Forms.TextBox();
             this.AddButton = new System.Windows.Forms.Button();
             this.ShowButton = new System.Windows.Forms.Button();
+            this.arraySizeTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ConfirmButton = new System.Windows.Forms.Button();
+            this.ReverseButton = new System.Windows.Forms.Button();
+            this.SumButton = new System.Windows.Forms.Button();
+            this.CopyButton = new System.Windows.Forms.Button();
+            this.DuplicateButton = new System.Windows.Forms.Button();
+            this.UniqueButton = new System.Windows.Forms.Button();
+            this.MaxMinButton = new System.Windows.Forms.Button();
+            this.OddEvenButton = new System.Windows.Forms.Button();
+            this.AscendingSortButton = new System.Windows.Forms.Button();
+            this.InsertButton = new System.Windows.Forms.Button();
+            this.DescendingSortButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // displayRichTextBox
             // 
-            this.displayRichTextBox.Location = new System.Drawing.Point(31, 117);
+            this.displayRichTextBox.Location = new System.Drawing.Point(38, 125);
             this.displayRichTextBox.Name = "displayRichTextBox";
-            this.displayRichTextBox.Size = new System.Drawing.Size(735, 281);
+            this.displayRichTextBox.Size = new System.Drawing.Size(728, 122);
             this.displayRichTextBox.TabIndex = 0;
             this.displayRichTextBox.Text = "";
             // 
             // inputTextBox
             // 
-            this.inputTextBox.Location = new System.Drawing.Point(31, 75);
+            this.inputTextBox.Location = new System.Drawing.Point(451, 96);
             this.inputTextBox.Name = "inputTextBox";
-            this.inputTextBox.Size = new System.Drawing.Size(252, 22);
+            this.inputTextBox.Size = new System.Drawing.Size(125, 22);
             this.inputTextBox.TabIndex = 1;
             // 
             // AddButton
             // 
-            this.AddButton.Location = new System.Drawing.Point(290, 73);
+            this.AddButton.Location = new System.Drawing.Point(582, 95);
             this.AddButton.Name = "AddButton";
             this.AddButton.Size = new System.Drawing.Size(75, 24);
             this.AddButton.TabIndex = 2;
@@ -61,21 +75,170 @@
             // 
             // ShowButton
             // 
-            this.ShowButton.Location = new System.Drawing.Point(691, 76);
+            this.ShowButton.Location = new System.Drawing.Point(706, 94);
             this.ShowButton.Name = "ShowButton";
-            this.ShowButton.Size = new System.Drawing.Size(75, 22);
+            this.ShowButton.Size = new System.Drawing.Size(60, 25);
             this.ShowButton.TabIndex = 3;
             this.ShowButton.Text = "Show";
             this.ShowButton.UseVisualStyleBackColor = true;
             this.ShowButton.Click += new System.EventHandler(this.ShowButton_Click);
             // 
+            // arraySizeTextBox
+            // 
+            this.arraySizeTextBox.Location = new System.Drawing.Point(158, 97);
+            this.arraySizeTextBox.Name = "arraySizeTextBox";
+            this.arraySizeTextBox.Size = new System.Drawing.Size(59, 22);
+            this.arraySizeTextBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Select Array Size";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(344, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Input Elements";
+            // 
+            // ConfirmButton
+            // 
+            this.ConfirmButton.Location = new System.Drawing.Point(223, 95);
+            this.ConfirmButton.Name = "ConfirmButton";
+            this.ConfirmButton.Size = new System.Drawing.Size(75, 25);
+            this.ConfirmButton.TabIndex = 5;
+            this.ConfirmButton.Text = "Confirm";
+            this.ConfirmButton.UseVisualStyleBackColor = true;
+            this.ConfirmButton.Click += new System.EventHandler(this.ConfirmButton_Click);
+            // 
+            // ReverseButton
+            // 
+            this.ReverseButton.Location = new System.Drawing.Point(12, 252);
+            this.ReverseButton.Name = "ReverseButton";
+            this.ReverseButton.Size = new System.Drawing.Size(78, 27);
+            this.ReverseButton.TabIndex = 6;
+            this.ReverseButton.Text = "Reverse";
+            this.ReverseButton.UseVisualStyleBackColor = true;
+            this.ReverseButton.Click += new System.EventHandler(this.ReverseButton_Click);
+            // 
+            // SumButton
+            // 
+            this.SumButton.Location = new System.Drawing.Point(98, 252);
+            this.SumButton.Name = "SumButton";
+            this.SumButton.Size = new System.Drawing.Size(53, 27);
+            this.SumButton.TabIndex = 7;
+            this.SumButton.Text = "Sum";
+            this.SumButton.UseVisualStyleBackColor = true;
+            this.SumButton.Click += new System.EventHandler(this.SumButton_Click);
+            // 
+            // CopyButton
+            // 
+            this.CopyButton.Location = new System.Drawing.Point(157, 252);
+            this.CopyButton.Name = "CopyButton";
+            this.CopyButton.Size = new System.Drawing.Size(57, 27);
+            this.CopyButton.TabIndex = 8;
+            this.CopyButton.Text = "Copy";
+            this.CopyButton.UseVisualStyleBackColor = true;
+            this.CopyButton.Click += new System.EventHandler(this.CopyButton_Click);
+            // 
+            // DuplicateButton
+            // 
+            this.DuplicateButton.Location = new System.Drawing.Point(220, 252);
+            this.DuplicateButton.Name = "DuplicateButton";
+            this.DuplicateButton.Size = new System.Drawing.Size(84, 27);
+            this.DuplicateButton.TabIndex = 9;
+            this.DuplicateButton.Text = "Duplicate";
+            this.DuplicateButton.UseVisualStyleBackColor = true;
+            this.DuplicateButton.Click += new System.EventHandler(this.DuplicateButton_Click);
+            // 
+            // UniqueButton
+            // 
+            this.UniqueButton.Location = new System.Drawing.Point(310, 252);
+            this.UniqueButton.Name = "UniqueButton";
+            this.UniqueButton.Size = new System.Drawing.Size(70, 27);
+            this.UniqueButton.TabIndex = 10;
+            this.UniqueButton.Text = "Unique";
+            this.UniqueButton.UseVisualStyleBackColor = true;
+            this.UniqueButton.Click += new System.EventHandler(this.UniqueButton_Click);
+            // 
+            // MaxMinButton
+            // 
+            this.MaxMinButton.Location = new System.Drawing.Point(386, 253);
+            this.MaxMinButton.Name = "MaxMinButton";
+            this.MaxMinButton.Size = new System.Drawing.Size(77, 27);
+            this.MaxMinButton.TabIndex = 10;
+            this.MaxMinButton.Text = "Max-Min";
+            this.MaxMinButton.UseVisualStyleBackColor = true;
+            this.MaxMinButton.Click += new System.EventHandler(this.MaxMinButton_Click);
+            // 
+            // OddEvenButton
+            // 
+            this.OddEvenButton.Location = new System.Drawing.Point(469, 253);
+            this.OddEvenButton.Name = "OddEvenButton";
+            this.OddEvenButton.Size = new System.Drawing.Size(81, 27);
+            this.OddEvenButton.TabIndex = 10;
+            this.OddEvenButton.Text = "Odd-Even";
+            this.OddEvenButton.UseVisualStyleBackColor = true;
+            this.OddEvenButton.Click += new System.EventHandler(this.OddEvenButton_Click);
+            // 
+            // AscendingSortButton
+            // 
+            this.AscendingSortButton.Location = new System.Drawing.Point(556, 253);
+            this.AscendingSortButton.Name = "AscendingSortButton";
+            this.AscendingSortButton.Size = new System.Drawing.Size(79, 27);
+            this.AscendingSortButton.TabIndex = 10;
+            this.AscendingSortButton.Text = "Sort-Asc";
+            this.AscendingSortButton.UseVisualStyleBackColor = true;
+            this.AscendingSortButton.Click += new System.EventHandler(this.SortButton_Click);
+            // 
+            // InsertButton
+            // 
+            this.InsertButton.Location = new System.Drawing.Point(728, 253);
+            this.InsertButton.Name = "InsertButton";
+            this.InsertButton.Size = new System.Drawing.Size(63, 27);
+            this.InsertButton.TabIndex = 11;
+            this.InsertButton.Text = "Insert";
+            this.InsertButton.UseVisualStyleBackColor = true;
+            this.InsertButton.Click += new System.EventHandler(this.InsertButton_Click);
+            // 
+            // DescendingSortButton
+            // 
+            this.DescendingSortButton.Location = new System.Drawing.Point(641, 253);
+            this.DescendingSortButton.Name = "DescendingSortButton";
+            this.DescendingSortButton.Size = new System.Drawing.Size(81, 27);
+            this.DescendingSortButton.TabIndex = 12;
+            this.DescendingSortButton.Text = "Sort-Des";
+            this.DescendingSortButton.UseVisualStyleBackColor = true;
+            this.DescendingSortButton.Click += new System.EventHandler(this.DescendingSortButton_Click);
+            // 
             // ArrayUi
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 327);
+            this.Controls.Add(this.DescendingSortButton);
+            this.Controls.Add(this.InsertButton);
+            this.Controls.Add(this.AscendingSortButton);
+            this.Controls.Add(this.OddEvenButton);
+            this.Controls.Add(this.MaxMinButton);
+            this.Controls.Add(this.UniqueButton);
+            this.Controls.Add(this.DuplicateButton);
+            this.Controls.Add(this.CopyButton);
+            this.Controls.Add(this.SumButton);
+            this.Controls.Add(this.ReverseButton);
+            this.Controls.Add(this.ConfirmButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.ShowButton);
             this.Controls.Add(this.AddButton);
+            this.Controls.Add(this.arraySizeTextBox);
             this.Controls.Add(this.inputTextBox);
             this.Controls.Add(this.displayRichTextBox);
             this.Name = "ArrayUi";
@@ -92,6 +255,20 @@
         private System.Windows.Forms.TextBox inputTextBox;
         private System.Windows.Forms.Button AddButton;
         private System.Windows.Forms.Button ShowButton;
+        private System.Windows.Forms.TextBox arraySizeTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button ConfirmButton;
+        private System.Windows.Forms.Button ReverseButton;
+        private System.Windows.Forms.Button SumButton;
+        private System.Windows.Forms.Button CopyButton;
+        private System.Windows.Forms.Button DuplicateButton;
+        private System.Windows.Forms.Button UniqueButton;
+        private System.Windows.Forms.Button MaxMinButton;
+        private System.Windows.Forms.Button OddEvenButton;
+        private System.Windows.Forms.Button AscendingSortButton;
+        private System.Windows.Forms.Button InsertButton;
+        private System.Windows.Forms.Button DescendingSortButton;
     }
 }
 
--- UnderstandingArray/ArrayUi.cs
+++ UnderstandingArray/ArrayUi.cs
@@ -12,14 +12,17 @@ namespace UnderstandingArray
 {
     public partial class ArrayUi : Form
     {
-        
+
         public ArrayUi()
         {
             InitializeComponent();
         }
-        int size = 4;
+        int size = 0;
         int index = 0;
-        int[] number; 
+        int[] number;
+        int[] copiedNumber;
+        string message;
+        int count;
         private void ArrayUi_Load(object sender, EventArgs e)
         {
             //int[] number = new int[10];
@@ -34,35 +37,316 @@ namespace UnderstandingArray
             //    messsage = messsage + "Value of Index " + index + " is: " + number[index] + "\n";
             //}
             //displayRichTextBox.Text = messsage;
-            number = new int[size];
+
+            displayRichTextBox.Text = "Input the number of elements to store in the array and confirm";
         }
 
         private void AddButton_Click(object sender, EventArgs e)
         {
             displayRichTextBox.Clear();
-           
-                if (index != size)
+
+
+            if (index != size)
+            {
+                if (String.IsNullOrEmpty(inputTextBox.Text))
+                {
+                    displayRichTextBox.Text = "Please, input value";
+                }
+                else
                 {
                     number[index] = Convert.ToInt32(inputTextBox.Text);
-                    displayRichTextBox.Text = "Value " + inputTextBox.Text + " is added to storage";
+
+                    message = message + "Value entered at " + index + " index is: " + inputTextBox.Text + "\n";
+                    displayRichTextBox.Text = message;
                     index++;
+                    inputTextBox.Clear();
                 }
-                else displayRichTextBox.Text = "Storage is full";
-                    
-            
+
+
+            }
+            else displayRichTextBox.Text = "Storage is full";
+
+
         }
 
         private void ShowButton_Click(object sender, EventArgs e)
         {
             string message = "Elements are: ";
-            for(int index=0; index<size; index++)
+            for (int index = 0; index < size; index++)
             {
-                if (number[index] != null)
+                if (number[index] != 0)
                 {
-                    message = message + number[index]+ " ";
+                    message = message + number[index] + " ";
                 }
                 displayRichTextBox.Text = message;
             }
+        }
+
+        private void ConfirmButton_Click(object sender, EventArgs e)
+        {
+            
+            size = 0;
+            
+            if (String.IsNullOrEmpty(arraySizeTextBox.Text))
+            {
+                MessageBox.Show("Please give array size");
+            }
+            else
+            {
+                size = Convert.ToInt32(arraySizeTextBox.Text);
+                displayRichTextBox.Text = "Confirmed, Now you can store " + size + " element ";
+                number = new int[size];
+                arraySizeTextBox.Clear();
+            }
+
+        }
+
+        private void ReverseButton_Click(object sender, EventArgs e)
+        {
+            string message = "Elements are: ";
+            string reverseMessage = "Reverse elements are: ";
+            for (int index = 0; index < size; index++)
+            {
+                if (number[index] != 0)
+                {
+                    message = message + number[index] + ", ";
+                }
+
+            }
+            for (int index = size - 1; index > -1; index--)
+            {
+                if (number[index] != null)
+                {
+                    reverseMessage = reverseMessage + number[index] + ", ";
+                }
+
+            }
+            displayRichTextBox.Text = message + "\n" + reverseMessage;
+        }
+
+        private void SumButton_Click(object sender, EventArgs e)
+        {
+            string message = "Elements are: ";
+            string sumMessage = "Sum of all elements stored in the array is: ";
+            int sum = 0;
+            for (int index = 0; index < size; index++)
+            {
+                if (number[index] != 0)
+                {
+                    message = message + number[index] + ", ";
+                }
+
+            }
+            for (int index = 0; index < size; index++)
+            {
+                if (number[index] != 0)
+                {
+                    sum = sum + number[index];
+
+                }
+
+            }
+            displayRichTextBox.Text = message + "\n" + sumMessage + sum;
+        }
+
+        private void CopyButton_Click(object sender, EventArgs e)
+        {
+            string firstArryMessage = "The elements stored in the first array are: ";
+            for (int index = 0; index < size; index++)
+            {
+                if (number[index] != 0)
+                {
+                    firstArryMessage = firstArryMessage + number[index] + " , ";
+                }
+
+            }
+
+            string secondArrayMessage = "The elements copied into the second array are: ";
+            copiedNumber = new int[number.Length];
+            for (int index = 0; index < number.Length; index++)
+            {
+                copiedNumber[index] = number[index];
+                secondArrayMessage = secondArrayMessage + copiedNumber[index] + " , ";
+
+            }
+            displayRichTextBox.Text = firstArryMessage + "\n" + secondArrayMessage;
+        }
+
+        private void DuplicateButton_Click(object sender, EventArgs e)
+        {
+
+
+            string allElements = AllElements();
+            int totalDuplicateCount = 0;
+            int[] elementChecked = new int[size];
+
+            for (int i = 0; i < number.Length; i++)
+            {
+                if (number[i] != elementChecked[i])
+                {
+                    for (int j = i + 1; j < number.Length; j++)
+                    {
+                        if (number[i] == number[j])
+                        {
+                            totalDuplicateCount++;
+                            for (int k = 0; k < size; k++)
+                            {
+                                elementChecked[k] = number[i];
+                            }
+                        }
+                    }
+                }
+
+            }
+
+            displayRichTextBox.Text = allElements + "\nTotal number of duplicate elements found in the array is: " + totalDuplicateCount;
+
+
+        }
+        private string AllElements()
+        {
+            string message = "Elements are: ";
+            for (int index = 0; index < size; index++)
+            {
+                if (number[index] != 0)
+                {
+                    message = message + number[index] + ", ";
+                }
+
+            }
+            return message;
+        }
+
+        private void UniqueButton_Click(object sender, EventArgs e)
+        {
+
+            int[] copyNumber = number;
+            int[] uniqueNumber = new int[number.Length];
+            for (int i = 0; i < number.Length; i++)
+            {
+                count = 0;
+                for (int j = 0; j < copyNumber.Length; j++)
+                {
+                    if (number[i] == copyNumber[j])
+                    {
+                        count++;
+                    }
+                }
+                if (count == 1)
+                {
+                    uniqueNumber[i] = number[i];
+
+                }
+            }
+            string uniqueNumberInfo = "Unique Elements are: ";
+            for (int k = 0; k < uniqueNumber.Length; k++)
+            {
+                if (uniqueNumber[k] != 0)
+                {
+                    uniqueNumberInfo = uniqueNumberInfo + " " + uniqueNumber[k];
+                }
+
+            }
+            displayRichTextBox.Text = AllElements() + "\n" + uniqueNumberInfo;
+
+
+        }
+
+        private void MaxMinButton_Click(object sender, EventArgs e)
+        {
+            int max= number[0];
+            int min = number[0];
+            for (int i = 0; i < number.Length; i++)
+            {
+                
+                if (number[i] > max)
+                {
+                    max = number[i];
+                }
+                if(number[i]< min)
+                {
+                    min = number[i];
+                }
+                
+            }
+            string messaage= "Max: " + max + "\nMin: " + min;
+            displayRichTextBox.Text = messaage;
+        }
+
+        private void OddEvenButton_Click(object sender, EventArgs e)
+        {
+            string oddNumber = "Odd Number: ";
+            string evenNumber = "Even Number: ";
+            for (int i = 0; i < number.Length; i++)
+            {
+
+                if (number[i]%2==0)
+                {
+                    evenNumber = evenNumber +" "+ number[i];
+                }
+                else oddNumber = oddNumber +" "+ number[i];
+            }
+            displayRichTextBox.Text =AllElements()+"\n"+ oddNumber + "\n" + evenNumber;
+        }
+
+        private void SortButton_Click(object sender, EventArgs e)
+        {
+            int temp;
+            for(int i=0; i < number.Length; i++)
+            {
+                for(int j=i+1; j<number.Length; j++)
+                {
+                    if (number[i] < number[j])
+                    {
+
+                    }else if (number[i] > number[j])
+                    {
+                        temp = number[i];
+                        number[i] = number[j];
+                        number[j] = temp;
+                    }
+                }
+            }
+            displayRichTextBox.Text = AllElements();
+        }
+
+        private void DescendingSortButton_Click(object sender, EventArgs e)
+        {
+            int temp;
+            for (int i = 0; i < number.Length; i++)
+            {
+                for (int j = i + 1; j < number.Length; j++)
+                {
+                    if (number[i] > number[j])
+                    {
+
+                    }
+                    else if (number[i] < number[j])
+                    {
+                        temp = number[i];
+                        number[i] = number[j];
+                        number[j] = temp;
+                    }
+                }
+            }
+            displayRichTextBox.Text = AllElements();
+        }
+
+        private void InsertButton_Click(object sender, EventArgs e)
+        {
+           
+            if (number[index] == size)
+            {
+                displayRichTextBox.Text = "Storage is full";
+            }else
+            {
+                for(int i=0; i<number.Length; i++)
+                {
+
+                }
+                number[index] = Convert.ToInt32( inputTextBox.Text);
+            }
+            index++;
         }
     }
 }
